d7504531cbfee7fbf9d970ab0b0478b64ee6e31a
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-11-16 22:51:41 -0300

    Database e Profiler

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -29,6 +28,17 @@ class DefaultController extends AbstractController
         return $this->render('index.html.twig',[
             'title' => 'Postagem Teste',
             'posts' => $posts
+        ]);
+    }
+
+    /**
+     * @Route("/post/{slug}")
+     */
+    public function single($slug)
+    {
+
+        return $this->render('single.html.twig',[
+            'slug' => $slug
         ]);
     }
 }
